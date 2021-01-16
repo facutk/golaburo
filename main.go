698f3100f3e5a2a9580c530079698b867f22748b
jtkaczyszyn <jtkaczyszyn@elementum.com>
2021-01-16 00:20:02 -0300

    add hello world

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World!")
+	})
+
+	http.ListenAndServe(os.Getenv("HOST")+":"+os.Getenv("PORT"), nil)
+}
