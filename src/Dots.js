c9f52c8ee8b2a94926c0174b86d3e6e087976b17
jtkaczyszyn <jtkaczyszyn@elementum.com>
2021-01-19 01:52:36 -0300

    change animation

--- src/Dots.js
+++ src/Dots.js
@@ -3,20 +3,30 @@ import styled from 'styled-components'
 const Dots = styled.span`
   &::after {
     display: inline-block;
-    animation: ellipsis 1.25s infinite;
+    animation: ellipsis 1s infinite;
     content: ".";
     width: 1em;
     text-align: left;
+    white-space: pre;
   }
   @keyframes ellipsis {
     0% {
       content: ".";
     }
-    33% {
+    16% {
       content: "..";
     }
-    66% {
+    32% {
       content: "...";
+    }
+    48% {
+      content: " ..";
+    }
+    64% {
+      content: "  .";
+    }
+    80% {
+      content: "";
     }
   }
 `;
