2e2db2a29eac404dca5463f2cec356f969d19451
jtkaczyszyn <jtkaczyszyn@elementum.com>
2021-02-15 15:29:07 -0300

    move all tailwind to uikit

--- uikit/src/Button.js
+++ uikit/src/Button.js
@@ -6,7 +6,7 @@ const Button = ({
   return (
     <button
       onClick={onClick}
-      className='bg-yellow-600'
+      className='bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
     >
       {children}
     </button>
